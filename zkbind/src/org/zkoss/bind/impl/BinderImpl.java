2ed39a1e2d63360471b7ed980ae96dbb7b47cb49
Karel Cemus <karel.cemus@gmail.com>
2012-08-23 11:26:09 +0200

    ZK-159 Changed visibility of BinderImpl#getCommandMethod
     - visibility changed from private to protected to allow to call it from inhereted classes

--- zkbind/src/org/zkoss/bind/impl/BinderImpl.java
+++ zkbind/src/org/zkoss/bind/impl/BinderImpl.java
@@ -1519,7 +1519,7 @@ public class BinderImpl implements Binder,BinderCtrl,Serializable{
 	}
 
 	
-	private Method getCommandMethod(Class<?> clz, String command) {
+	protected Method getCommandMethod(Class<?> clz, String command) {
 		Map<String,CachedItem<Method>> methods = _commandMethodCache.get(clz);
 		synchronized(_commandMethodCache){
 			methods = _commandMethodCache.get(clz);//check again
